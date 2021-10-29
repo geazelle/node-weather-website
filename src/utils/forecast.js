0a08dc2cf4ed4f41f6f72b15e499611b60215c91
Cabiles, Geazelle Joy <geazelle.joy.cabiles@accenture.com>
2021-10-29 19:13:53 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require(`request`)
+
+const forecast = (latitude, longtitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=587527990929b0d41ed420f118b1110d&query=` + latitude + `,` + longtitude + `&units=s`
+
+    request({ url: url, json: true }, (error, { body }) => {
+        if (error) {
+            callback(`Unable to connect to weather service! `, undefined)
+        } else if (body.error) {
+            callback(`Unable to find location.`, undefined)
+        } else {
+            callback(undefined, `It is currently ` + body.current.temperature + ` degrees out. It feels like ` + body.current.feelslike + ` degrees out.`)
+        }
+    })
+}
+
+module.exports = forecast
